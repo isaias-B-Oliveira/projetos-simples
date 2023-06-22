979beaac8f2efcc4b58414a7cd24a0bec4178ff6
isaias-B-oliveira <joseisaias123bezerra@gmail.com>
2023-06-22 15:17:24 -0300

    adionando mais projetos modal-simples

---
+++ MODAL_SIMPLES/script.js
@@ -0,0 +1,19 @@
+
+const BTM = document.querySelector('.button-abri');
+const Modal = document.querySelector('dialog');
+const BTMfechar = document.querySelector('.button-fechar');
+
+function Abrir() {
+    BTM.addEventListener('click', () => {
+        Modal.showModal();
+    });
+};
+Abrir();
+
+function Fechar() {
+    BTMfechar.addEventListener('click', () => {
+        Modal.close();
+    });
+};
+Fechar();
+
